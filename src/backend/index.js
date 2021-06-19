9de9b2cd4021c4e9d7f0a8a3bbc693ab92c141cc
Nicolás Salvo <nicosalvo@gmail.com>
2021-06-19 19:13:16 -0300

    [FIX] create endpoint

--- src/backend/index.js
+++ src/backend/index.js
@@ -15,89 +15,116 @@ const path = require('path');
 const devicesPath = path.join(__dirname, 'devices.json');
 
 // to parse application/json
-app.use(express.json()); 
+app.use(express.json());
 // to serve static files
 app.use(express.static('/home/node/app/static/'));
 
 //=======[ Main module code ]==================================================
 
 // Ejercicio 4
-app.get('/devices/', function(req, res, next) {
-    res.json(devices).status(200);
+app.get('/devices/', function (req, res, next) {
+  res.status(200).json(devices);
 });
 
 // Ejercicio 5
-app.get('/devices/:id', function(req, res, next) {
-    const id = req.params.id;
-    const deviceIndex = getDeviceIndexById(id);
-    if (deviceIndex > -1) {
-        res.send(200).json(devices[deviceIndex]);
-    } else {
-        res.status(404).send(`No se encontró ningún dispositivo con id:${id}`);
-    }
+app.get('/devices/:id', function (req, res, next) {
+  const id = req.params.id;
+  const deviceIndex = getDeviceIndexById(id);
+  if (deviceIndex > -1) {
+    res.status(200).json(devices[deviceIndex]);
+  } else {
+    res
+      .status(404)
+      .send({ message: `No se encontró ningún dispositivo con id:${id}` });
+  }
 });
 
 // Ejercicio 6
-app.post('/devices/', function(req, res, next) {
-    const {id, state} = req.body;
-    const deviceIndex = getDeviceIndexById(id);
-    if (deviceIndex > -1) {
-        devices[deviceIndex].state = state;
-        writeToFile(devices, devicesPath);
-        res.status(204).send(`Cambio realizado: ${JSON.stringify(devices[deviceIndex])}`);
-    } else {
-        res.status(404).send(`No se encontró ningún dispositivo con id:${id}`);
-    }
-});
-
-app.post('/devices/create', function(req, res, next) {
-    const {name, description, state, type} = req.body;
-
-    if (!name || !description || !state || !type) {
-        res.status(400).send('Falta un parámetro en la consulta');
-    }
-
-    const id = devices.length + 1;
-    const newDevice = {
-        id,
-        name,
-        description,
-        state,
-        type
-    }
-    devices.push(newDevice);
+app.put('/devices/:id', function (req, res, next) {
+  const id = req.params.id;
+  const { name, description, state, type } = req.body;
+  const deviceIndex = getDeviceIndexById(id);
+  if (deviceIndex > -1) {
+    devices[deviceIndex].state = state;
     writeToFile(devices, devicesPath);
-    res.status(200).json('Se creó el dispositivo satisfactoriamente');
-})
-
-app.delete('/devices/:id', function(req, res, next) {
-    const id = req.params.id;
-    const deviceIndex = getDeviceIndexById(id);
-    if (deviceIndex > -1) {
-        const device = devices.splice(deviceIndex, 1);
-        // devices = devices.filter(device => Number(deviceIndex) !== device.id);
-        writeToFile(devices, devicesPath);
-        res.status(200).send(`Se eliminó el dispositivo ${device[0].name}`);
-    } else {
-        res.status(404).send(`No se encontró ningún dispositivo con id:${id}`);
-    }
+    res
+      .status(200)
+      .send(`Cambio realizado: ${JSON.stringify(devices[deviceIndex])}`);
+  } else {
+    res
+      .status(404)
+      .send({ message: `No se encontró ningún dispositivo con id:${id}` });
+  }
 });
 
-app.listen(PORT, function(req, res) {
-    console.log("NodeJS API running correctly");
+app.post('/devices/create', function (req, res, next) {
+  const { name, description, state, type } = req.body;
+
+  if (
+    !isDefined(name) ||
+    !isDefined(description) ||
+    !isDefined(state) ||
+    !isDefined(type)
+  ) {
+    return res
+      .status(400)
+      .json({ message: 'Falta un parámetro en la consulta' });
+  }
+
+  const id = devices.length + 1;
+  const newDevice = {
+    id,
+    name,
+    description,
+    state,
+    type,
+  };
+  devices.push(newDevice);
+  // writeToFile(devices, devicesPath);
+  res
+    .status(201)
+    .send({ message: 'Se creó el dispositivo satisfactoriamente' });
+});
+
+app.delete('/devices/:id', function (req, res, next) {
+  const id = req.params.id;
+  const deviceIndex = getDeviceIndexById(id);
+  if (deviceIndex > -1) {
+    const device = devices.splice(deviceIndex, 1);
+    // devices = devices.filter(device => Number(deviceIndex) !== device.id);
+    writeToFile(devices, devicesPath);
+    res
+      .status(200)
+      .send({ message: `Se eliminó el dispositivo ${device[0].name}` });
+  } else {
+    res
+      .status(404)
+      .send({ message: `No se encontró ningún dispositivo con id:${id}` });
+  }
+});
+
+app.listen(PORT, function (req, res) {
+  console.log('NodeJS API running correctly');
 });
 
 function getDeviceById(id) {
-    return devices.find(device => Number(id) === device.id);
+  return devices.find((device) => Number(id) === device.id);
 }
 
 function getDeviceIndexById(id) {
-    return devices.findIndex(device => Number(id) === device.id);
+  return devices.findIndex((device) => Number(id) === device.id);
 }
 
 function writeToFile(file, path) {
-    const filestringified = JSON.stringify(file);
-    fs.writeFileSync(path, filestringified)
+  const filestringified = JSON.stringify(file);
+  fs.writeFileSync(path, filestringified);
+}
+
+function isDefined(item) {
+  if (item === undefined) {
+    return false;
+  }
+  return true;
 }
 
 //=======[ End of file ]=======================================================
--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,32 +1,32 @@
 //Ejercicio 9
 type DeviceInt = {
-    id: number;
-    name: string;
-    description: string;
-    state: number;
-    type: number;
-}
+  id: number;
+  name: string;
+  description: string;
+  state: number;
+  type: number;
+};
 
 // Ejercicio 10
 class ViewMainPage {
-    public myFramework: MyFramework = new MyFramework();
-    showDevices(list: Array<DeviceInt>): void {
-        let ul: HTMLElement = this.myFramework.getElementById('deviceList');
-        
-        // clear list
-        while (ul.firstChild) {
-            ul.removeChild(ul.firstChild);
-        }
+  public myFramework: MyFramework = new MyFramework();
+  showDevices(list: Array<DeviceInt>): void {
+    let ul: HTMLElement = this.myFramework.getElementById('deviceList');
 
-        // poblar lista
-        list.forEach(device => {
-            // create li element
-            let element = document.createElement('li')
-            let checked = !!device.state ? 'checked' : '';
-            // add class to element
-            element.classList.add("collection-item", "avatar")
-            // add rest of content
-            element.innerHTML = `
+    // clear list
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+
+    // poblar lista
+    list.forEach((device) => {
+      // create li element
+      let element = document.createElement('li');
+      let checked = !!device.state ? 'checked' : '';
+      // add class to element
+      element.classList.add('collection-item', 'avatar');
+      // add rest of content
+      element.innerHTML = `
                 <img src="images/yuna.jpg" alt="" class="circle">
                 <span class="title">${device.name}</span>
                 <p>${device.description} <br>
@@ -41,166 +41,164 @@ class ViewMainPage {
                     </label>
                 </div>
             `;
-            // add li element to ul
-            ul.appendChild(element)
-        });
-    }
+      // add li element to ul
+      ul.appendChild(element);
+    });
+  }
 
-    getSwitchStateById(id: string): boolean {
-        let deviceSwitch: HTMLInputElement = this.myFramework.getElementById(id) as HTMLInputElement;
-        return deviceSwitch.checked;
-    }
+  getSwitchStateById(id: string): boolean {
+    let deviceSwitch: HTMLInputElement = this.myFramework.getElementById(
+      id,
+    ) as HTMLInputElement;
+    return deviceSwitch.checked;
+  }
 }
 
-class Main implements EventListenerObject, GETResponseListener, POSTResponseListener {
-    public myFramework: MyFramework;
-    public viewMainPage: ViewMainPage;
-    constructor(){}
+class Main
+  implements EventListenerObject, GETResponseListener, POSTResponseListener
+{
+  public myFramework: MyFramework;
+  public viewMainPage: ViewMainPage;
+  constructor() {}
 
-    main(): void {
-        console.log('Mensaje del metodo main()')
-        this.myFramework = new MyFramework();
-
-        // Ejercicio 8
-        this.myFramework.requestGET('./Devices.txt', this);
-
-        this.viewMainPage = new ViewMainPage();
-    }
-
-    mostrarLista() {
-        let usersList: Array<User> = new Array<User>()
-        let user1: User = new User(1, 'a@a.com', true)
-        let user2: User = new User(2, 'b@b.com', false)
-        let user3: User = new User(3, 'c@c.com', true)
-
-        usersList.push(user1)
-        usersList.push(user2)
-        usersList.push(user3)
-        
-        for(let user of usersList) {
-            user.printInfo();
-        }
-    }
-    
-    // Ejercicio 4-2
-    mostrarUsers(users: Array<User>): void {
-        for( let user of users) {
-            console.log(user);
-        }
-    }
-
-    // Ejercicio 5
-    evento(ev: Event): void {
-        console.log('se hizo click!');
-        console.log(this); // devuelve el nodo con el elemento boton
-    }
-
-    handleEvent(event: Event): void {
-        console.log(this); // muestra el objeto instanciado
-        console.log(event.target);
-        
-        // Ejercicio 7
-        let boton: HTMLElement = this.myFramework.getElementByEvent(event);
-
-        if(boton.textContent === 'Listar') {
-            // this.mostrarLista();
-            let xhr: XMLHttpRequest = new XMLHttpRequest();
-            xhr.onreadystatechange = () => {
-                if (xhr.readyState == 4) {
-                    if (xhr.status == 200) {
-                        console.log("Llego la respuesta!!!!");
-                        console.log(xhr.responseText);
-                        // let parrafo = this.myFramework.getElementById("lista");
-                        // parrafo.innerHTML = xhr.responseText;
-                        this.myFramework.requestGET("http://localhost:8000/devices/", this);
-                    } else {
-                        alert("error!!")
-                    }
-                    
-                }
-                
-            }
-            xhr.open("GET","http://localhost:8000/devices", true)
-            xhr.send();
-            return;
-        } 
-        if (boton.id.includes("dev_")){
-            console.log(boton.id);
-            let checked = this.viewMainPage.getSwitchStateById(boton.id);
-            let state = checked ? 1 : 0;
-            let id = boton.id.slice(4);
-            this.myFramework.requestPost("http://localhost:8000/devices/", {id, state}, this);
-            return;
-        }
-
-        else {
-            alert('No hay funcion para ese boton');
-        }
-    }
+  main(): void {
+    console.log('Mensaje del metodo main()');
+    this.myFramework = new MyFramework();
 
     // Ejercicio 8
-    handleGETResponse(status: number, response: string): void {
-        if (status === 200) {
-            // Ejercicio 9
-            let data: DeviceInt[] = JSON.parse(response);
-            // console.log(`Parsed response: ${JSON.stringify(data, null, 2)}`);
-            this.viewMainPage.showDevices(data);
-            
-            let element: HTMLElement;
-            data.forEach(data => {
-                console.log(data);
-                 element = this.myFramework.getElementById("dev_" + data.id);
-                 element.addEventListener("click", this);
-            })
-            // let parrafo = this.myFramework.getElementById("lista");
-            // parrafo.innerHTML = response;
-        } else {
-            console.log(`Status: ${status}`);
-        }
+    this.myFramework.requestGET('./Devices.txt', this);
+
+    this.viewMainPage = new ViewMainPage();
+  }
+
+  mostrarLista() {
+    let usersList: Array<User> = new Array<User>();
+    let user1: User = new User(1, 'a@a.com', true);
+    let user2: User = new User(2, 'b@b.com', false);
+    let user3: User = new User(3, 'c@c.com', true);
+
+    usersList.push(user1);
+    usersList.push(user2);
+    usersList.push(user3);
+
+    for (let user of usersList) {
+      user.printInfo();
     }
+  }
 
-    handlePOSTResponse(status: number, response: string): void {
-        alert(`status: ${status} - response: ${response}`);
+  // Ejercicio 4-2
+  mostrarUsers(users: Array<User>): void {
+    for (let user of users) {
+      console.log(user);
     }
+  }
 
-    
-}
+  // Ejercicio 5
+  evento(ev: Event): void {
+    console.log('se hizo click!');
+    console.log(this); // devuelve el nodo con el elemento boton
+  }
 
-
-
-
-window.onload = function () {
-    const main = new Main()
-    // inicializo myFramework
-    main.main();
-    // main.mostrarLista();
-    let usersList: Array<User> = new Array<User>()
-    let user1: User = new User(1, 'a@a.com', true)
-    let user2: User = new User(2, 'b@b.com', false)
-    let user3: User = new User(3, 'c@c.com', true)
-
-    usersList.push(user1)
-    usersList.push(user2)
-    usersList.push(user3)
-
-    // Ejercicio 4-2
-    // main.mostrarUsers(usersList);
-
-    let btnListar: HTMLElement = main.myFramework.getElementById('btnListar');
-    btnListar.textContent = 'Listar'
-    let btnLimpiar: HTMLElement = main.myFramework.getElementById('btnLimpiar');
-    btnLimpiar.textContent = 'Limpiar'
-
-    // Ejercio 5
-    // boton.addEventListener('click', main.evento);
-
-    // Ejercicio 6
-    btnListar.addEventListener('click', main);
+  handleEvent(event: Event): void {
+    console.log(this); // muestra el objeto instanciado
+    console.log(event.target);
 
     // Ejercicio 7
-    btnLimpiar.addEventListener('click', main)
+    let boton: HTMLElement = this.myFramework.getElementByEvent(event);
 
+    if (boton.textContent === 'Listar') {
+      // this.mostrarLista();
+      let xhr: XMLHttpRequest = new XMLHttpRequest();
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState == 4) {
+          if (xhr.status == 200) {
+            console.log('Llego la respuesta!!!!');
+            console.log(xhr.responseText);
+            // let parrafo = this.myFramework.getElementById("lista");
+            // parrafo.innerHTML = xhr.responseText;
+            this.myFramework.requestGET('http://localhost:8000/devices/', this);
+          } else {
+            alert('error!!');
+          }
+        }
+      };
+      xhr.open('GET', 'http://localhost:8000/devices', true);
+      xhr.send();
+      return;
+    }
+    if (boton.id.includes('dev_')) {
+      console.log(boton.id);
+      let checked = this.viewMainPage.getSwitchStateById(boton.id);
+      let state = checked ? 1 : 0;
+      let id = boton.id.slice(4);
+      this.myFramework.requestPost(
+        'http://localhost:8000/devices/',
+        { id, state },
+        this,
+      );
+      return;
+    } else {
+      alert('No hay funcion para ese boton');
+    }
+  }
+
+  // Ejercicio 8
+  handleGETResponse(status: number, response: string): void {
+    if (status === 200) {
+      // Ejercicio 9
+      let data: DeviceInt[] = JSON.parse(response);
+      // console.log(`Parsed response: ${JSON.stringify(data, null, 2)}`);
+      this.viewMainPage.showDevices(data);
+
+      let element: HTMLElement;
+      data.forEach((data) => {
+        console.log(data);
+        element = this.myFramework.getElementById('dev_' + data.id);
+        element.addEventListener('click', this);
+      });
+      // let parrafo = this.myFramework.getElementById("lista");
+      // parrafo.innerHTML = response;
+    } else {
+      console.log(`Status: ${status}`);
+    }
+  }
+
+  handlePOSTResponse(status: number, response: string): void {
+    alert(`status: ${status} - response: ${response}`);
+  }
 }
+
+window.onload = function () {
+  const main = new Main();
+  // inicializo myFramework
+  main.main();
+  // main.mostrarLista();
+  let usersList: Array<User> = new Array<User>();
+  let user1: User = new User(1, 'a@a.com', true);
+  let user2: User = new User(2, 'b@b.com', false);
+  let user3: User = new User(3, 'c@c.com', true);
+
+  usersList.push(user1);
+  usersList.push(user2);
+  usersList.push(user3);
+
+  // Ejercicio 4-2
+  // main.mostrarUsers(usersList);
+
+  let btnListar: HTMLElement = main.myFramework.getElementById('btnListar');
+  btnListar.textContent = 'Listar';
+  let btnLimpiar: HTMLElement = main.myFramework.getElementById('btnLimpiar');
+  btnLimpiar.textContent = 'Limpiar';
+
+  // Ejercio 5
+  // boton.addEventListener('click', main.evento);
+
+  // Ejercicio 6
+  btnListar.addEventListener('click', main);
+
+  // Ejercicio 7
+  btnLimpiar.addEventListener('click', main);
+};
 
 // window.addEventListener('load', function() {
 //     main()
