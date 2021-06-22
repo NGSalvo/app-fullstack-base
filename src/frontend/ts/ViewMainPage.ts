cd1678a1f2efa926c8868c9cd600ddc93d3472f0
Nicolás Salvo <nicosalvo@gmail.com>
2021-06-22 11:06:25 -0300

    [MOD] agrega componente de rango en funcion del tipo de dispositivo

--- src/frontend/ts/ViewMainPage.ts
+++ src/frontend/ts/ViewMainPage.ts
@@ -13,7 +13,6 @@ class ViewMainPage {
     list.forEach((device) => {
       // Crea elementos <li>
       let element = document.createElement('li');
-      let checked = !!device.state ? 'checked' : '';
       // Agrega clases al elemento
       element.classList.add('collection-item', 'avatar');
       // Agrega id
@@ -22,20 +21,15 @@ class ViewMainPage {
       element.innerHTML = `
                 <img src="images/yuna.jpg" alt="" class="circle">
                 <span class="title">${device.name}</span>
-                <p>${device.description} <br>
-                    ${device.type}
-                </p>
-                <div class="switch secondary-content">
-                    <label>
-                        Off
-                        <input id="status_${device.id}" type="checkbox" ${checked}>
-                        <span class="lever"></span>
-                        On
-                    </label>
-                    <a href="#modal1" class="waves-effect waves-light modal-trigger"><i id="edit_${device.id}" class="material-icons edit_dev">edit</i></a>
-                    <a href="#modal2" class="waves-effect waves-light modal-trigger"><i id="delete_${device.id}" class="material-icons delete_dev">delete</i></a>
-                </div>
+                <p>${device.description}</p>
             `;
+      element.innerHTML = element.innerHTML + this.getComponentByType(device);
+
+      // Agrega el boton editar y eliminar
+      let actionButtons = `<div class="center-align"><a href="#modal1" id="edit_${device.id}" class="btn-flat edit_dev waves-effect waves-light modal-trigger">Editar</a>
+       <a href="#modal2" id="delete_${device.id}"class="btn-flat delete_dev waves-effect waves-light modal-trigger">Eliminar</a></div>
+       `;
+      element.innerHTML = element.innerHTML + actionButtons;
       // Agrega el elemento <li> al elemento <ul>
       ul.appendChild(element);
     });
@@ -47,5 +41,30 @@ class ViewMainPage {
       id,
     ) as HTMLInputElement;
     return deviceSwitch.checked;
+  }
+
+  getComponentByType(device: DeviceInt): string {
+    let checked = !!device.state ? 'checked' : '';
+    let element: string;
+    switch (device.type) {
+      case 1:
+        element = `<p class="range-field secondary-content">
+      <input type="range" id="status_${device.id}" min="0" max="100" />
+      </p>`;
+        break;
+      case 2:
+        element = `<div class="switch secondary-content">
+                    <label>
+                        Off
+                        <input id="status_${device.id}" type="checkbox" ${checked}>
+                        <span class="lever"></span>
+                        On
+                    </label>
+                </div>`;
+        break;
+      default:
+        element = '';
+    }
+    return element;
   }
 }
